96b886786560dd9cca4e4740e3a019283a9c0e66
Stephen Joyner <stephen@rundeck.com>
2022-05-12 15:01:24 -0500

    Update tests

--- service-test.js
+++ service-test.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 
-export default function (data) {
+export default function (d) {
     http.get('http://localhost:8080/actuator/health', {
         tags: {name:'Actuator - healthcheck'}
     });
