3c364068fa21573af3db72d9ab594f6aa392f8db
Stephen Joyner <stephen@rundeck.com>
2022-05-18 15:18:53 -0500

    Fixed function

--- service-test.js
+++ service-test.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 
-export default function (d) {
+export default function () {
     http.get('http://localhost:8080/actuator/health', {
         tags: {name:'Actuator - healthcheck'}
     });
